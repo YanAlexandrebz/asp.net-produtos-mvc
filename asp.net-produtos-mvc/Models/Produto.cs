37448002bf8f412a33a0f6ef6114831768536e04
Yan Alexandre <rm77740@fiap.com.br>
2021-02-04 13:41:16 -0300

    Adicionando novas funcionalidades

--- asp.net-produtos-mvc/Models/Produto.cs
+++ asp.net-produtos-mvc/Models/Produto.cs
@@ -6,17 +6,20 @@ using System.Threading.Tasks;
 
 namespace asp.net_produtos_mvc.Models
 {
-    public class Categoria
+    public class Produto
     {
-        public int Id { get; set; }
-        [Display(Name = "NomeCategoria")]
-        [Required(ErrorMessage = "O campo descricao e obrigatorio")]
 
+        public int Id { get; set; }
+        [Display(Name = "NomeProduto")]
+        [Required(ErrorMessage = "O campo descricao e obrigatorio")]
         public String Descricao { get; set; }
 
-        public List<Produto> Produtos { get; set; }
+        [Range(1, 10, ErrorMessage = "A quantidade deve estar entre 1 e 10")]
+        public int Quantidade { get; set; }
 
+        [Range(1, 10, ErrorMessage = "o id deve estar entre 1 e 100")]
+        public int IdCategoria { get; set; }
 
-
+        public Categoria Categoria { get; set; }
     }
 }
