4cddb86a214053b30843067c10d98479c48c6b90
Yan Alexandre <rm77740@fiap.com.br>
2021-02-04 12:28:30 -0300

    Adicionar arquivos de projeto.

---
+++ asp.net-produtos-mvc/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp.net_produtos_mvc.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        public String Descricao { get; set; }
+
+
+
+    }
+}
